7b836f7574c41b0da433c98caa769e6233cd7c9f
Lyudmila <milo4kamoya@gmail.com>
2022-05-15 11:52:03 -0700

    Add new image on button click

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,28 @@
 import React from "react";
+import images from "../memesData";
 
 export default function Form() {
+
+  const [memeImg, setMemeImg] = React.useState('');
+
+  function getImage(e) {
+    e.preventDefault();
+    const memesArr = images.data.memes;
+    const randomIndex = Math.floor(Math.random() * images.data.memes.length);
+    const url = memesArr[randomIndex].url;
+    //console.log('index:', randomIndex, 'url:', url);
+
+    setMemeImg(url);
+  }
+
   return (
     <main>
       <form className="form">
         <input className="form--input" type='text' placeholder="Enter key word" />
         <input className="form--input" type='text' placeholder="Enter key phrase" />
-        <input className="form--button" type='submit' value='Get a new meme image  🖼' />
+        <input onClick={getImage} className="form--button" type='submit' value='Get a new meme image  🖼' />
       </form>
+      <img className="meme-img" src={memeImg} alt="Meme"/>
     </main>
   )
 }
